f56019ea398b1d8b6a4abe89597ec3cd87c21c74
Rob Weeks <weeks@radiantnova.com>
2025-04-10 06:33:59 -0400

    finished

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from stats import *
-def main():
-    book_path = "books/frankenstein.txt"
-    #book_text = read_book(book_path)
-    #print(count_words(book_text))
-    #print(count_chars(book_text))
-    #print(chars_to_sorted_list(count_chars(book_text)))
-    print_report(book_path)
+import sys
 
+def main():
+    if len(sys.argv) > 1:
+        book_path = sys.argv[1]
+        print_report(book_path)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
